82bf73d98f347d96a7e6983da517239dadf65f7b
Kamil Sieniawski <kamil.sieniawski@gmail.com>
2019-04-30 19:43:52 +0200

    add logger to middleware service

--- middleware/main.rb
+++ middleware/main.rb
@@ -1,1 +1,10 @@
-puts "Hello Ruby!"
+require 'logging'
+
+log_path = './logs/middleware.log'
+logger = Logging.logger['middleware']
+logger.level = :info
+
+logger.add_appenders \
+    Logging.appenders.file(log_path)
+
+logger.info "just some friendly advice"
